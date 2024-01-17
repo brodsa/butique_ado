9761bed38e45b0a08ee660f7046e9c7b71f90ab9
brodsa <brodinova@gmail.com>
2024-01-17 19:46:04 +0000

    feat: add order history

--- checkout/views.py
+++ checkout/views.py
@@ -3,13 +3,17 @@ from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+
+from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
+from bag.contexts import bag_contents
+
 import stripe
 import json
 
-from .forms import OrderForm
-from .models import Order, OrderLineItem
-from products.models import Product
-from bag.contexts import bag_contents
 
 @require_POST
 def cache_checkout_data(request):
@@ -32,11 +36,9 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    # Logic for POST 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
 
-        # get the data from the request
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -48,11 +50,8 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
-        # create instance of the form 
-        order_form = OrderForm(form_data)
 
-        # if the form is valid, save the order and create each line item 
-        # and save it into the data base
+        order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
@@ -78,7 +77,6 @@ def checkout(request):
                                 product_size=size,
                             )
                             order_line_item.save()
-                # in case the product is not found delete the order
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your bag wasn't found in our database. "
@@ -87,13 +85,9 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
 
-            # if the user wants to save the profile in to session
+            # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-
-            # finally the user is redirected to the success checkout
             return redirect(reverse('checkout_success', args=[order.order_number]))
-        
-        # the form is not valid
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
@@ -103,7 +97,6 @@ def checkout(request):
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
-        # Caculate total from shopping bag
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
@@ -113,43 +106,78 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing. \
-                Did you forget to set it in your environment?')
+        # Attempt to prefill the form with any info the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
-        order_form = OrderForm()
-        template = 'checkout/checkout.html'
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret': intent.client_secret,
-        }
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
-        return render(request, template, context)
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
 
 def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
-    # save whether or not the user wants to save the info - will be used when creating the user profile
     save_info = request.session.get('save_info')
-
-    # get the order for the order number and use the order email
-    # in the toast message
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
-    # delete the bag as it is not nedeed 
     if 'bag' in request.session:
         del request.session['bag']
 
-    # set template and context
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
     }
 
-    # render the template with context
     return render(request, template, context)
