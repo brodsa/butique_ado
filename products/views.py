a1b85ee1aebd81e34bfee50446027bf8c2fa9ed3
brodsa <brodinova@gmail.com>
2024-01-18 17:34:53 +0000

    feat: implement deletng products

--- products/views.py
+++ products/views.py
@@ -71,12 +71,10 @@ def add_product(request):
     """ Add a product to the store """
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
-        print('form fill out')
         if form.is_valid():
-            print('form valid')
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             print('form not valid')
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
@@ -113,3 +111,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
